3e591b034ab8bb2c1be18dbb32bfe0acb8f10d78
TUYIMENYE <tuyimenyeaimee@gmail.com>
2024-02-15 15:16:29 +0200

    changes in index file
    added bootstrap link

--- src/index.php
+++ src/index.php
@@ -1,2 +1,3 @@
 <?php
 echo"<h3> we are dealing with PHP</h3>";
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
